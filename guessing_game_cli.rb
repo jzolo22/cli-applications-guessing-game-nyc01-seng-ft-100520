f03d292484c86077bfbccc548aef424ce3326864
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-26 17:31:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,7 +14,7 @@ def run_guessing_game
   puts "Type a number between 1 and 6."
   user_guess = gets.chomp
   if user_guess == 
-  if user_guess == random_number
+  if user_guess == random_number.to_s
     puts "You guessed the correct number!"
   elsif user_guess == "exit"
     puts "Goodbye!"
