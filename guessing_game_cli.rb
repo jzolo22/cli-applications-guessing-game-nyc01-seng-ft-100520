28dc6287c181613c7ab1d5c21d74221f4deafb65
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-26 17:24:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,9 @@
 # Code your solution here!
 
 
-def guess_prompt
-  puts "Type a number between 1 and 6."
-end
+# def guess_prompt
+#   puts "Type a number between 1 and 6."
+# end
   
   
 def run_guessing_game
